b6c183b49ba0b144b5845c4f7616af359d5e828e
artemisp <artemisp@seas.upenn.edu>
2022-06-20 13:01:34 -0400

    Update downloader

---
+++ download.sh
@@ -0,0 +1,13 @@
+
+# data
+wget https://drive.google.com/file/d/1iTf0GrMIIo_xzy-0je9L-YHiCH-ROJe-/view?usp=sharing -O data.zip
+unzip data.zip
+
+# output
+wget https://drive.google.com/file/d/13VL4UPv9H-1CMdne6HZ3iBibJ-PNB6kI/view?usp=sharing -O output.zip
+unzip output.zip
+
+
+# noun2ngram_count
+wget https://drive.google.com/file/d/1rAe04WU96IfUxu3f7UfPqARAIPd9kERG/view?usp=sharing -O noun2prop2count_ngram.zip
+unzip  noun2prop2count_ngram.zip -d models/ngram/
