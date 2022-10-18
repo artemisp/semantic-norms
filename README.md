df31f4e3d9664a96263180efaa2fbbe23b6801e4
artemisp <artemisp@seas.upenn.edu>
2022-10-17 21:43:05 -0400

    Add data download links

--- README.md
+++ README.md
@@ -9,16 +9,12 @@ pip install -r requirements.txt
 
 ## Data download
 
+Follow the [instructions](https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html) to install the AWS CLI to download the data.
+
 Run the download bash file from the projercts home directory 
 ```
 bash download.sh
-```
-This script will not download the raw images. To download the raw images for each of the datasets, use the links below:
-* Concept Properties: [here](https://drive.google.com/uc?export=download&id=1KGAb31doDMIQCiqi3zg0464LF37kE7Xa)
-* Feature Norms: [here](https://drive.google.com/uc?export=download&id=1TueIWc-sgZB3a6H9VFo_LQJ3dNCzgGm0)
-* Memory Colors: [here](https://drive.google.com/uc?export=download&id=1dtBrTAbZbgpNP2O4BqPcgwPTVBBfBKdU)
-
-and place them in `data/datasets/{dataset_name}/images/bing_images`. 
+``` 
 
 ## Replicate experiments
 
--- download.sh
+++ download.sh
@@ -1,24 +1,13 @@
 
 # data
-wget https://drive.google.com/uc?export=download&id=198Y1uFamiN2-0XTjykfVy59zuCNJfnbP -O data.zip
+wget https://semantic-norms-data.s3.amazonaws.com/data.zip -O data.zip
 unzip data.zip
 
 
 # output
-wget  https://drive.google.com/uc?export=download&id=10LBuiah8rih5tb4QIpYtxhNC9fgr11Bn -O output.zip
+wget  https://semantic-norms-data.s3.amazonaws.com/output.zip -O output.zip
 unzip output.zip
 
 # noun2ngram_count
-wget https://drive.google.com/uc?export=download&id=1xnbowRO_DzyyyJLysc1HkQeMW8zRiJWb -O noun2prop2count_ngram.zip
+wget https://semantic-norms-data.s3.amazonaws.com/noun2prop2count_ngram.p -O noun2prop2count_ngram.zip
 unzip  noun2prop2count_ngram.zip -d models/ngram/
-
-
-## uncomment below to download raw images. 
-# wget https://drive.google.com/uc?export=download&id=1KGAb31doDMIQCiqi3zg0464LF37kE7Xa -O bing_images.zip
-# unzip  bing_images.zip -d data/datasets/concept_properties/images/
-
-# wget https://drive.google.com/uc?export=download&id=1TueIWc-sgZB3a6H9VFo_LQJ3dNCzgGm0 -O noun2prop2count_ngram.zip
-# unzip  bing_images.zip -d data/datasets/feature_norms/images/
-
-# wget https://drive.google.com/uc?export=download&id=1dtBrTAbZbgpNP2O4BqPcgwPTVBBfBKdU -O noun2prop2count_ngram.zip
-# unzip  bing_images.zip -d data/datasets/memory_color/images/
